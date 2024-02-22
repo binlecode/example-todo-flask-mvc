388c895890cbc2fe396ff9b4115131ba1630cea6
Bin Le <bin.le.code@gmail.com>
2024-02-22 09:32:25 -0500

    better comment

--- mvc/model.py
+++ mvc/model.py
@@ -3,13 +3,17 @@
 
 import uuid
 
-import flask_sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import DateTime
 from sqlalchemy.orm import backref, validates
 from sqlalchemy.sql import func
 
+# flask_sqlalchemy's SQLAlchemy integrates flask with both sqlalchemy's 
+# core (Table) and orm (Model, which is based on declarative base).
 db = SQLAlchemy()
+
+# for model relationships in sqlalchemy, see:
+# https://docs.sqlalchemy.org/en/20/orm/basic_relationships.html
 
 # m-to-m mapping table b/t todos and users
 # an m-to-m mapping table should not be a model, but simply a table
@@ -23,12 +27,18 @@ assignments = db.Table(
 
 # to check table DDL for Todo model, in flask console, run Todo.__table__
 
+# flask_sqlalchemy's Model is based on sqlalchemy's declarative base, which
+# is a convenient base class for declarative ORM table defintion.
+# 
 
 class Todo(db.Model):
     # flask-sqlalchemy sets table name to be the lower-camel-case of class name
     # it would be 'todo' for this entity
     # we would like to set the table name to be plural, aka 'todos'
     __tablename__ = "todos"
+    # pass down to Table constructor args to set low-level settings such as 
+    # schema name if needed
+    # __table_args__ = {'schema': 'public'}
 
     # auto timestamping
     # use sqlalchemy's func.now() to ask DB to calculate the timestamp itself
